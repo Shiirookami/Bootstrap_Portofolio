2d9a2e9e0a0ce9edaa14a490678d8aeae52092b6
Shiirookami <rzkir818@gmail.com>
2022-11-10 07:30:29 +0700

    add data versi 2

--- js/aos.js
+++ js/aos.js
@@ -1,16 +1,35 @@
 
 
-    AOS.init();
-    let navbar = document.getElementById('navbar-custom');
+AOS.init();
+let navbar = document.getElementById('navbar-custom');
 
-    window.addEventListener('scroll', function(){
-        let scroll = window.scrollY;
+window.addEventListener('scroll', function(){
+    let scroll = window.scrollY;
 
-        if (scroll > 50) {
-            navbar.classList.add('bg-light');
-            navbar.classList.remove('navbar-dark');
-        } else {
-            navbar.classList.remove('bg-light');
-            navbar.classList.add('navbar-dark');
-        }
-    });
+    if (scroll > 50) {
+        navbar.classList.add('bg-light');
+        navbar.classList.remove('navbar-dark');
+    } else {
+        navbar.classList.remove('bg-secondary');
+        navbar.classList.add('navbar-dark');
+    }
+});
+
+// Get Pedeih Button
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// Scroll to Back
+function topedeih() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
